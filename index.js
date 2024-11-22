748875b659e593e48d2ffdf49560e4465115e55c
unknown <kuldeepgangarekar2001@gmail.com>
2024-11-22 18:55:20 +0530

    This will give you the even no array in the given array

---
+++ index.js
@@ -0,0 +1,9 @@
+function evenNumber(arr){
+    let evenNum =[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2 === 0) evenNum.push(arr[i]);
+    }
+    console.log(evenNum);
+}
+
+evenNumber([2,3,4,6,7,5,8,9,0]);
